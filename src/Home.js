9e808207311226b4058729d100f08b5cb7ca4f66
unknown <cocotte243@gmail.com>
2021-01-04 15:38:24 +0100

    login sur page home

--- src/Home.js
+++ src/Home.js
@@ -1,17 +1,19 @@
-
+import'./Login';
 
 function Hello(){
     return(
         <div className="Home">
             <h1>Hello 2 iTech</h1>
             <a
-         className="Login-link"
-         href="./Login"
-         target="_blank"
-         rel="noopener noreferrer"
-       >
-         Connectez-vous!
-       </a>
+           
+                className="Login-link"
+                href="./Login"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                Connectez-vous!
+            </a>
+            <h2>Bienvenue  </h2>
         </div>
         
     );
--- src/Login.js
+++ src/Login.js
@@ -1,20 +1,69 @@
+// import React from 'react';
+import { BrowserRouter as Router, Redirect, Route, Switch } from
+'react-router-dom';
+import { useHistory } from "react-router-dom";
 function Login() {
+    const history = useHistory();
+
+    const routeChange = () =>{ 
+      let path = "/Home"; 
+      history.push(path);
+    }
     return (
         <div className="Login">
-            <form>
+            <form onSubmit={postDataForm}>
                 <label>
                     Nom :
-                    <input type="text" name="name" />
+                    <input type="text" name="name" id="name" />
                 </label>
                 <label>
                     Password :
-                    <input type="text" name="name" />
+                    <input type="password" name="password" id="password" />
                 </label>
-                <input type="submit" value="Envoyer" />
+                <input onClick={routeChange} type="submit" value="Envoyer" />
             </form>
         </div>
 
 
     )
 }
+function postDataForm(){
+    let name = document.getElementById('name');
+    // let password = document.getElementById('password');
+    let nameValue = name.value;
+    // let passwordValue = password.value;
+    console.log(name.value)
+    return nameValue
+}
 export default Login;
+// export default class Login extends React.PureComponent {
+//     // 4 elements importants
+//     state = {
+    
+//     }
+//     static propTypes = {
+    
+//     }
+//     constructor(props) {
+        
+//     }
+//     render() {
+//         return (
+//             <div className="Login">
+//                 <form>
+//                     <label>
+//                         Nom :
+//                         <input type="text" name="name" />
+//                     </label>
+//                     <label>
+//                         Password :
+//                         <input type="text" name="name" />
+//                     </label>
+//                     <input type="submit" value="Envoyer" />
+//                 </form>
+//             </div>
+    
+    
+//         );
+//     }
+//    }
